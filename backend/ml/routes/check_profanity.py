10eb12dc1a6de9e3b004d02ae712b5d097d9e634
Fajar <fajar.nur.h@protonmail.com>
2021-06-06 03:53:11 +0700

    Read data from user id and display with json format

--- backend/ml/routes/check_profanity.py
+++ backend/ml/routes/check_profanity.py
@@ -1,30 +1,22 @@
 import os
-import re
+import csv
 from flask import request, jsonify
 from utils import status_code
 from utils.resp_out import err_resp, new_resp
-
-def get_user_id(dirr):
-    for f in os.listdir(dirr):
-        user_compile = re.compile('[0-9a-f]{32}\Z', re.I)
-        user_match = re.match(user_compile, f)
-        user_id = user_match.group(0)
-        
-    return user_id
+from utils.helper import get_user_id, open_dir
 
 def init_list_profanity_routes(app):
     @app.route("/api/v1/user/check", methods=["GET"])
     def check_profanity_route():
         if request.method == "GET":
             listBad = []
-            rootDir = os.path.abspath(os.getcwd())
-            assetsDir = os.path.join(rootDir,"assets")
-            userId = get_user_id(assetsDir) 
-            with open(os.path.join(assetsDir, userId), "r+") as f:
-                for line in f:
-                    sline = line.rstrip()
-                    listBad.append({"text":sline,"id":userId,"date":"08-04-2021"})
-            
+            userId = get_user_id()
+            with open(os.path.join(open_dir('assets'), userId), "r+") as f:
+                reader = csv.reader(f, delimiter=",")
+                line = 0
+                for row in reader:
+                    listBad.append({"id":row[0], "test":row[1], "date":row[2]})
+
             return (
                 jsonify(new_resp("success send data", {"texts": listBad}, status_code.CREATED)),
                 status_code.CREATED,
